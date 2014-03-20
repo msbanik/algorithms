5cb58797796a9d4ae2777cc3313659dd3cc3c5b2
Madhusudan Banik <msbanik@gmail.com>
2014-03-19 21:42:34 -0700

    reformat structure

---
+++ dynamic/longest_subsequence.py
@@ -0,0 +1,30 @@
+# algorithm to find the longest subsequence 
+def longest_subsequece(src, dest):
+	m = len(src) + 1
+	n = len(dest) + 1
+	# memorize the longest subsequence between src[:i] and dest[:j]
+	mem = [[0 for _ in xrange(n)] for _ in xrange(m)]
+	for i in xrange(1, m):
+		for j in xrange(1, n):
+			if src[i-1] == dest[j-1]:
+				mem[i][j] = 1 + mem[i-1][j-1]
+			else:
+				mem[i][j] = max(mem[i-1][j], mem[i][j-1])
+
+	print_da(mem, src, dest)
+
+# for printing the array
+def print_da(mem, src, dest):
+	m, n = len(src), len(dest)
+	for i in xrange(1,m):
+		mem[i][0] = src[i-1]
+	for i in xrange(1,n):
+		mem[0][i] = dest[i-1]
+	mem[0][0] = ' '
+	for m in mem:
+		for j in m:
+			print j,
+		print ''
+	print ''
+
+longest_subsequece('abby', 'abbywi')
---
+++ dynamic/rod_cutting.py
@@ -0,0 +1,23 @@
+# Dynamic programming solution for rod cutting problem
+
+# length of rod
+length = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# price for rod of length i
+price = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+
+# cut rod of length n and return total price
+def cut_rod(price, n):
+	# mem[i] will store the price of rod length i
+	mem = [0] * n
+	for l in xrange(1, n + 1):
+		m = price[l-1]
+		for s in xrange(1, l):
+			# we already have a solution for subproblem
+			m = max(m, mem[s-1] + mem[l-s-1])
+		# memorize
+		mem[l-1] = m
+	print mem
+	return mem[n-1]
+
+# [1, 5, 8, 10, 13, 17, 18, 22, 25, 30]
+cut_rod(price, 10)
